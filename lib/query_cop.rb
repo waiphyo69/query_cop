ad82405c29edaf7efcaa86441cff8bd8f575f44b
Wai Phyo Maung <waiphyo300@gmail.com>
2018-08-19 21:39:36 -0700

    cleanup

--- lib/query_cop.rb
+++ lib/query_cop.rb
@@ -1,6 +1,6 @@
 require_relative "query_cop/query_tracker"
-require "query_cop/active_record_adapter"
-require "query_cop/rspec_helper"
+require_relative "query_cop/active_record_adapter"
+require_relative "query_cop/rspec_helper"
 
 ActiveRecord::ConnectionAdapters::AbstractAdapter.send(:prepend, QueryCop::ActiveRecordAdapter) if Rails.env.test?
 
